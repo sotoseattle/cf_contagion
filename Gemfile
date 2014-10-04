29379fe28dce33c2c0a6f01469f4f505d6bcbdaa
Javier Soto <sotoseattle@gmail.com>
2014-10-04 13:20:34 -0700

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,32 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.1.6'
+
+gem 'coffee-rails', '~> 4.0.0'
+gem 'foundation-rails', '5.4.5'
+gem 'jquery-rails'
+gem 'sass-rails', '~> 4.0.3'
+gem 'turbolinks'
+gem 'uglifier', '>= 1.3.0'
+
+group :doc do
+  gem 'sdoc', '~> 0.4.0'
+end
+
+group :development do
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'launchy'
+  gem 'poltergeist'
+  gem 'pry-rails'
+  gem 'pry-rescue'
+  gem 'rubocop'
+  gem 'spring'
+  gem 'sqlite3'
+end
+
+group :test do
+  gem 'minitest-rails-capybara'
+end
+
+ruby '2.1.2'
---
+++ test/test_helper.rb
@@ -0,0 +1,17 @@
+# ENV['RAILS_ENV'] ||= 'test'
+Rails.env = 'test'    # This is the same as above (fjs6)
+puts "Current environment: #{Rails.env}"
+
+require File.expand_path('../../config/environment', __FILE__)
+require 'rails/test_help'
+require 'minitest/rails/capybara'
+require 'capybara/poltergeist'
+# require 'pry-rescue/minitest'
+
+class ActiveSupport::TestCase
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  fixtures :all
+
+  # Add more helper methods to be used by all tests here...
+  Capybara.javascript_driver = :poltergeist
+end
